dacf4467edb1079b85b122f276907c776b5fd64a
Urmat Kokonbaev <urmat.kokonbaev@pearson.com>
2025-07-29 00:56:51 +0600

    added abort controller and pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
+  const [page, setPage] = useState(1);
+
+  const abortController = useRef<AbortController | null>(null);
 
   useEffect(() => {
     const getData = async () => {
       setIsLoading(true);
       try {
+        abortController.current?.abort();
+        abortController.current = new AbortController();
         setIsError(false)
         const response = await fetch(
-          `${import.meta.env.VITE_API_URL}/posts?_limit=12`
+          `${import.meta.env.VITE_API_URL}/posts?_limit=12&_page=${page}`, {
+            signal: abortController.current.signal,
+          }
         );
         const data = await response.json();
         if (!response.ok) {
@@ -20,24 +27,29 @@ function App() {
         }
         setPosts(data);
         setIsLoading(false);
-      } catch (error) {
+      } catch (error: any) {
         console.error("Error fetching data:", error);
+        if (error.name === 'AbortError') {
+          console.log("Fetch aborted");
+          return;
+        }
         setIsError(true);
-        return;
       } finally {
         setIsLoading(false);
       }
     };
     getData();
-  }, []);
+  }, [page]);
 
   return (
     <>
+      <button onClick={() => setPage(page + 1)}>Next page</button>
+
       {isLoading && <p>Loading...</p>}
 
       {isError && <p>Error loading posts</p>}
 
-      {!isLoading && !isError && <ul>
+      {!isLoading && !isError && posts.length > 0 && <ul>
         {posts.map((item: any) => (
           <li key={item.id}>{item.title}</li>
         ))}
